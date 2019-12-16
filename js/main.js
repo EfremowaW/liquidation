77f6e273940ec5024de8ff3c008fbf7eaac2c67c
EfremowaW <54147056+EfremowaW@users.noreply.github.com>
2019-12-16 08:58:51 +0300

    Верстка секций "Получить консультацию" и "Преимущества"
    +Добавление формы в модальное окно

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+$(function(){
+	
+	
+
 $('.slider__inner').slick({
 	prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
 	nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
 	infinite: false
+});
+	
+
+$('input, select').styler();
+	
+	
 });
