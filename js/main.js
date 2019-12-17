d7bf164bc2647616c6ae32ef4b21dc3626bf343e
EfremowaW <54147056+EfremowaW@users.noreply.github.com>
2019-12-17 20:03:41 +0300

    Верстка секции "Новости компании"

--- js/main.js
+++ js/main.js
@@ -11,5 +11,14 @@ $('.slider__inner').slick({
 
 $('input, select').styler();
 	
+
+$('.news__prev-list').slick({
+  prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+	nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
+	infinite: false,
+  slidesToShow: 2,
+  slidesToScroll: 1
+});
 	
+
 });
